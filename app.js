23736363faf0af104e3ba11b973b2d48e89f5f25
Nikhil Bansal <nikbansal1998@gmail.com>
2022-03-29 23:26:56 +0530

    feat: URL generated
    Worked on generating the valid URL and appending the encrypted message as a hash value in the URL.
    Added HTML to display decrypted message which will only get displayed when there is a hash value present in the URL.

--- app.js
+++ app.js
@@ -6,5 +6,20 @@ const form = document.querySelector("form");
 form.addEventListener("submit", function(eventObj){
     eventObj.preventDefault();
     let encrypted_message = btoa(messageInput.value);
-    linkInput.value = encrypted_message;
+    linkInput.value = window.location.href + "#" + encrypted_message;
+    linkInput.select();
 });
+
+
+
+const msgLinkContainer = document.querySelector("#message-link-container");
+const decryptedMsgContainer = document.querySelector("#decrypted-message-container");
+
+if(window.location.hash)
+{
+    let hashValue = window.location.hash.replace("#", "");
+    msgLinkContainer.classList.add("hide");
+    decryptedMsgContainer.classList.remove("hide");
+    const p_element = document.querySelector("#decrypted-message-container > .msg-container > p");
+    p_element.innerText = `Here is your friend's secret message: ${atob(hashValue)}`;
+}
