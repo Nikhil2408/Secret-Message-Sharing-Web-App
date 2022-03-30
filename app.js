ee278e40533b4a705d5896edcc7d84975a47b8b9
Nikhil Bansal <nikbansal1998@gmail.com>
2022-03-30 08:05:47 +0530

    fix: fixed create form url dynamically

--- app.js
+++ app.js
@@ -15,11 +15,16 @@ form.addEventListener("submit", function(eventObj){
 const msgLinkContainer = document.querySelector("#message-link-container");
 const decryptedMsgContainer = document.querySelector("#decrypted-message-container");
 
+let actualURL = "";
 if(window.location.hash)
 {
     let hashValue = window.location.hash.replace("#", "");
+    actualURL = window.location.href.replace(window.location.hash, "");
+    console.log(actualURL);
     msgLinkContainer.classList.add("hide");
     decryptedMsgContainer.classList.remove("hide");
     const p_element = document.querySelector("#decrypted-message-container > .msg-container > p");
     p_element.innerText = `Here is your friend's secret message: ${atob(hashValue)}`;
+    const a_element = document.querySelector("#decrypted-message-container > .msg-container > a");
+    a_element.setAttribute("href", actualURL);
 }
